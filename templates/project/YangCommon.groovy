7b8809515acce574d006ef06b60f599540a29454
Michal Novák <it.novakmi@gmail.com>
2012-01-03 22:59:14 +0100

    added example of project generating several yang files

---
+++ templates/project/YangCommon.groovy
@@ -0,0 +1,55 @@
+/*
+Copyright (c) 2012 Michal Novak (bubbles.way@gmail.com)
+http://bitbucket.org/bubbles.way/yangbuilder
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+// This class contains reusable parts, it is not related to specific yang file
+class YangCommon {
+
+        static def buildHeader(builder, prefixName = null) {
+                builder.yngbuild("/* This yang file was generated with groovy YangBuilder on ${new Date().toString()}", indent: true)
+                builder.yngbuild('   see http://bitbucket.org/bubbles.way/yangbuilder */', indent: true)
+                builder.yngbuild('') // new line
+
+                if (prefixName) { // do not generate prefix for and namespace for submodules
+                        builder.namespace "http://bitbucket.org/bubbles.way/yangbuilder"
+                        builder.prefix prefixName
+                }
+                builder.yngbuild('')
+
+                builder.'import'('ietf-inet-types') {
+                        prefix 'inet'
+                }
+                builder.yngbuild('')
+        }
+
+        static def buildAddressPort(builder, kind = null) { //this is example how function can be used by the builder, parameters can be used
+                // in function all nodes have to be prefixed with 'builder.', except for child nodes
+                builder.yngbuild("// IPv4 or IPv6 address", indent: true)
+                builder.leaf("${kind ? kind + '-' : ''}address") { //output depends on parameters, not possible in yang
+                        type('inet:ip-address')
+                }
+                builder.yngbuild("// IP port", indent: true)
+                builder.leaf("${kind ? kind + '-' : ''}port") {
+                        type('uint16')
+                }
+        }
+}
---
+++ templates/project/buildYangs.groovy
@@ -0,0 +1,63 @@
+#!/usr/bin/env groovy
+
+/*
+copyright (c) 2012 michal novak (bubbles.way@gmail.com)
+http://bitbucket.org/bubbles.way/yangbuilder
+
+permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "software"), to deal
+in the software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the software, and to permit persons to whom the software is
+furnished to do so, subject to the following conditions:
+
+the above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the software.
+
+the software is provided "as is", without warranty of any kind, express or
+implied, including but not limited to the warranties of merchantability,
+fitness for a particular purpose and noninfringement. in no event shall the
+authors or copyright holders be liable for any claim, damages or other
+liability, whether in an action of contract, tort or otherwise, arising from,
+out of or in connection with the software or the use or other dealings in
+the software.
+*/
+
+//if you have internet connection, use groovy grab to get dependencies (may take some time for the first time to download jars)
+//run as ordinary groovy script with command 'groovy <scriptname>.groovy' (or as linux executable script './<scriptname>.groovy')
+//update nodebuilder, yangbuilder version numbers as needed
+@GrabResolver(name = 'bubbleswayrepo', root = 'https://github.com/bubblesway/bubbleswayrepo/raw/master/releases', m2compatible = true)
+@Grab(group = 'org.bitbucket.novakmi', module = 'nodebuilder', version = '0.2.0')
+@Grab(group = 'org.bitbucket.novakmi', module = 'yangbuilder', version = '0.0.1')
+
+//due to @Grab limitation in script, we have to have def ... after Grab, in our case we can just create builder
+// see http://groovy.codehaus.org/Grapes+and+grab()
+def builder = new org.bitbucket.novakmi.yangbuilder.YangBuilder(4) // create new builder, indent 4 (default is 2)
+
+if (args) {  // arguments passed can be processed this way
+        println "Following arguments have been passed:"
+        args.each {a ->
+                println(a)
+        }
+}
+
+/**
+ create List of classes that are related to generated yang files
+ Each class has to have static function 'getName()' returning name of yang file (without yang extension) and
+ static function  'buildYang' accepting empty builder as parameter
+*/
+def yangs = [
+    YangBgp,
+    YangBgpSubmodule,
+    YangOspf,
+]
+
+for (y in yangs) { // one can also use yangs.each {y ->
+        builder.reset() // empty builder
+        y.buildYang(builder)
+        print("Processing ${y.getName()} ...")
+        new File("${y.getName()}.yang").write(builder.getBuiltText()) //write to file
+        println("done")
+}
+
+
