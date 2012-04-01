43497c19ae3f6539015cc518246e23a72ccc6f40
bubbles.way <bubbles.way@gmail.com>
2012-04-01 23:07:58 +0200

    template example script for CompactYangPlugin

--- templates/scripts/example1CompactYang.groovy
+++ templates/scripts/example1CompactYang.groovy
@@ -1,5 +1,7 @@
 #!/usr/bin/env groovy
 
+import org.bitbucket.novakmi.yangbuilder.CompactYangPlugin
+
 /*
 Copyright (c) 2012 Michal Novak (bubbles.way@gmail.com)
 http://bitbucket.org/bubbles.way/yangbuilder
@@ -30,22 +32,20 @@ THE SOFTWARE.
 @Grab(group = 'org.bitbucket.novakmi', module = 'nodebuilder', version = '0.3.0')
 @Grab(group = 'org.bitbucket.novakmi', module = 'yangbuilder', version = '0.1.0')
 
-// This script template represents example of usage without any plugin
-def builder = new org.bitbucket.novakmi.yangbuilder.YangBuilder(4) // create new builder, indent 4 (default is 2)
+// This script template represents example of usage with Compact yang plugin (syntax is more different from yang, but more compact)
+// create new builder, pass plugin or list of plugins as constructor parameter,
+// default indent of 2 has to be specified if we pass plugin (or list of plugins) in constructor (other option is to use builder.registerPlugin(new CompactYangPlugin()))
+def builder = new org.bitbucket.novakmi.yangbuilder.YangBuilder(2, new CompactYangPlugin())
 
 //name of file to generate
-moduleName = "example1-module"   // do not use 'def' for script global variable
+moduleName = "example1-compact-module"   // do not use 'def' for script global variable
 
 def makeAddressPort(builder, kind = null) { //this is example how function can be used by the builder, parameters can be used
         // in function all nodes have to be prefixed with 'builder.', except for child nodes
         builder.yngbuild("// IPv4 or IPv6 address", indent: true)
-        builder.leaf("${kind ? kind + '-' : ''}address") { //output depends on parameters, not possible in yang
-                type('inet:ip-address')
-        }
+        builder.leaf("${kind ? kind + '-' : ''}address", type: 'inet:ip-address', description: "IPv4 or IPv6 address") //output depends on parameters, not possible in yang
         builder.yngbuild("// IP port", indent: true)
-        builder.leaf("${kind ? kind + '-' : ''}port") {
-                type('uint16')
-        }
+        builder.leaf("${kind ? kind + '-' : ''}port", type: 'uint16', description: "IP port")  //with compact plugin 'type' can be declared as param. of leaf. 'description' as param of element
 }
 
 builder.module(moduleName) {
@@ -56,47 +56,31 @@ builder.module(moduleName) {
         }
 
         yngbuild("/* This yang file was generated with groovy YangBuilder on ${new Date().toString()}", indent: true)
-        yngbuild('   see http://bitbucket.org/bubbles.way/yangbuilder */', indent: true)
-        yngbuild('') // new line
+        yngbuild('   see http://bitbucket.org/bubbles.way/yangbuilder */', indent: true, nl: true)  // 'nl:true adds new line (you can also use nl:1, etc.)
 
         namespace "http://bitbucket.org/bubbles.way/yangbuilder"
-        prefix "example1"
-        yngbuild('')
+        prefix("example1", nl: 1) // add new line
 
-        'import'('ietf-inet-types') {
-                prefix 'inet'
-        }
-        yngbuild('')
+        'import'('ietf-inet-types', prefix: 'inet', nl: 1) //with compact yang 'prefix' can be added as attribute of import, add new line after ending '}' of import
 
         makeGrouping() // this behaves in the same way as if content of the closure is written here
-
         yngbuild('')
 
         yngbuild("/* neighbor */", indent: true)
-        container('neighbor') {
+        container('neighbor', nl: 1) {
                 uses 'addressPort'  // yang grouping reuse
         }
-        yngbuild('')
 
         ['bgp', 'ospf', 'isis', 'rip'].each {k -> // create 3 containers in loop, not possible in yang
                 yngbuild("/* ${k} neighbor */", indent: true)
-                container("${k}-neighbor") {
+                container("${k}-neighbor", nl: 1) {
                         makeAddressPort(builder, k) // as if content of function is written here, yangbuilder reuse (not possible in yang)
                 }
-                yngbuild('')
         }
 
-        list('neighbors') { // key leaf can be attribute of list statement
-                description('List of neighbors')
-                key('neighbor')
-                leaf('neighbor') {
-                        description "neighbor IP4 or IPv6 address"
-                        type 'inet:ip-address'
-                }
-                leaf('name') {
-                        description "neighbor name"
-                        type 'string'
-                }
+        list('neighbors', key: 'neighbor', description: 'List of neighbors') { // key leaf can be attribute of list statement
+                leaf('neighbor', type: 'inet:ip-address', description: 'neighbor IP4 or IPv6 address')
+                leaf('name', type: 'string', description: 'neighbor name')
         }
 }
 
