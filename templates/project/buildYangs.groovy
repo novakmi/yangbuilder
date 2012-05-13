3d14afd863557186b97facf11d315fe0b21ddfe6
bubbles.way <bubbles.way@gmail.com>
2012-05-13 23:29:09 +0200

    license information updated

--- templates/project/buildYangs.groovy
+++ templates/project/buildYangs.groovy
@@ -1,27 +1,7 @@
 #!/usr/bin/env groovy
 
-/*
-copyright (c) 2012 michal novak (bubbles.way@gmail.com)
-http://bitbucket.org/bubbles.way/yangbuilder
-
-permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "software"), to deal
-in the software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the software, and to permit persons to whom the software is
-furnished to do so, subject to the following conditions:
-
-the above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the software.
-
-the software is provided "as is", without warranty of any kind, express or
-implied, including but not limited to the warranties of merchantability,
-fitness for a particular purpose and noninfringement. in no event shall the
-authors or copyright holders be liable for any claim, damages or other
-liability, whether in an action of contract, tort or otherwise, arising from,
-out of or in connection with the software or the use or other dealings in
-the software.
-*/
+//This is free software licensed under MIT License, see LICENSE file
+//(https://bitbucket.org/bubbles.way/yangbuilder/src/LICENSE)
 
 //if you have internet connection, use groovy grab to get dependencies (may take some time for the first time to download jars)
 //run as ordinary groovy script with command 'groovy <scriptname>.groovy' (or as linux executable script './<scriptname>.groovy')
